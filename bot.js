612ed844053b7cdbfb211d96cc8c8ad0dfb4ab2e
ssliteris <ssliteris@exadel.com>
2020-12-20 13:22:10 +0200

    feat/playing status

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const command = require('./app/commands');
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!!`);
     client.user.setActivity(`traveller rpg (${Math.ceil(Math.random() * 1000)})`, {
-        type: 'CUSTOM_STATUS'
+        type: 'PLAYING'
     })
 });
 
